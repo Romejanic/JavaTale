5909a5168dbb67f06afc163a3fee9d994125ffd4
Romejanic <romejanic@gmail.com>
2017-06-11 15:42:50 +1000

    App framework and GLFW window

--- src/com/romejanic/javatale/Javatale.java
+++ src/com/romejanic/javatale/Javatale.java
@@ -1,9 +1,46 @@
 package com.romejanic.javatale;
 
+import com.romejanic.javatale.gl.Window;
+
 public class Javatale {
 
-	public static void main(String[] args) {
+	public static final Javatale instance = new Javatale();
+	
+	private void init() throws Throwable {
+		System.out.println("Started up!");
+		Window.create();
+	}
+	
+	private void update() {
+		Window.update();
+	}
+	
+	private void destroy() {
+		Window.destroy();
+	}
+	
+	public void exit(int code) {
+		if(code == 0) System.out.println("Exiting safely!");
+		else if(code == -1) System.err.println("Exiting because of a crash!");
+		else System.err.println("Exiting because of an unknown issue! (code " + code + ")");
 		
+		destroy();
+		System.exit(code);
+	}
+	
+	public static void main(String[] args) {
+		try {
+			instance.init();
+			while(!Window.shouldClose()) {
+				instance.update();
+			}
+			instance.exit(0);
+		} catch(Throwable e) {
+			System.err.println("!! CRASHED !!");
+			System.err.print("Caused by ");
+			e.printStackTrace(System.err);
+			instance.exit(-1);
+		}
 	}
 	
 }
---
+++ src/com/romejanic/javatale/gl/Window.java
@@ -0,0 +1,79 @@
+package com.romejanic.javatale.gl;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.glfw.Callbacks;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+
+public class Window {
+
+	private static long window = NULL;
+	private static int width = 640;
+	private static int height = 480;
+	private static String title = "JavaTale";
+	
+	private static int fbWidth = 0;
+	private static int fbHeight = 0;
+	
+	public static void create() throws Exception {
+		GLFWErrorCallback.createPrint(System.err).set();
+		if(!glfwInit()) {
+			throw new RuntimeException("GLFW failed to initialize!");
+		}
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+		if(window == NULL) {
+			throw new RuntimeException("Window creation failed!");
+		}
+		GLFWVidMode vm = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(window, (vm.width()-width)/2, (vm.height()-height)/2);
+		glfwMakeContextCurrent(window);
+		glfwSetFramebufferSizeCallback(window, (win, w, h) -> {
+			fbWidth = w; fbHeight = h;
+		});
+		glfwShowWindow(window);
+		
+		IntBuffer wb = BufferUtils.createIntBuffer(1);
+		IntBuffer hb = BufferUtils.createIntBuffer(1);
+		glfwGetFramebufferSize(window, wb, hb);
+		fbWidth = wb.get();
+		fbHeight = hb.get();
+	}
+	
+	public static void update() {
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	public static void destroy() {
+		if(window != NULL) {
+			Callbacks.glfwFreeCallbacks(window);
+			glfwDestroyWindow(window);
+		}
+		glfwTerminate();
+	}
+	
+	public static int getFramebufferWidth() {
+		return fbWidth;
+	}
+	
+	public static int getFramebufferHeight() {
+		return fbHeight;
+	}
+	
+	public static boolean shouldClose() {
+		return glfwWindowShouldClose(window);
+	}
+	
+}
